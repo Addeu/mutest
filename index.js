a1ee3d8cc53684d64677c0f5105ac95271fcf92f
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-01-18 21:20:14 +0500

    Super basic functionality is added

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 //Here the app begins
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const fs = require('file-system');
+const text = fs.readFileSync(`test.txt`, `utf8`);
+const app = express();
 const port = 3000;
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Server is running');
-}
 
-const server = http.createServer(requestHandler);
-server.listen(port, (err) => {
+app.get('/', (request, response) => {
+  response.render('t-upload', {
+    here: text
+  })
+});
+
+app.listen(port, (err) => {
   if (err) {
     return console.log('something wrong', err)
   }
 
-  console.log('server is listening on port ${port}');
-})
+  console.log(`server is listening on port ${port}`);
+});
+
+app.engine('.hbs', exphbs ({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
