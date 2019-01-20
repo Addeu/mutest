0177599c6740b82e017e51170f07440a1c36c74f
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-01-20 18:53:38 +0500

    Base app renamed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
+const appRouter = require('./routes/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 const usrPath = path.join(__dirname, 'public');
@@ -9,23 +10,22 @@ const usrPath = path.join(__dirname, 'public');
 //Storage settings
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './uploads')
+    cb(null, './uploads');
   },
   filename: function(req, file, cb) {
-    cb(null, file.fieldname + '.txt')
+    cb(null, file.fieldname + '.txt');
   }
 });
 const upload = multer({storage: storage});
 
 //Server
+app.use('/upload', appRouter);
 app.use(express.static(usrPath));
-app.post('/upload', upload.single('quiz'), (req, res) => {
-  req.end();
-});
+
 
 app.listen(port, (err) => {
   if (err) {
-    return console.log('something wrong', err)
+    return console.log('something wrong', err);
   }
 
   console.log(`server is listening on port ${port}`);
