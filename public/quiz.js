b365461484378f206d8c924a7e35e0d80dfbae13
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-01-27 14:48:08 +0500

    Materialize and modulize
    1. quizzling engine enhanced
    2. unlimited number of questions is now supported but proper work is not 
    guaranteed
    3. material design (cdn version) added

--- public/quiz.js
+++ public/quiz.js
@@ -1,15 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('/api/quizes', {method: 'get'}).then(res => res.json()).then(data => {
+  fetch('/api/quizes', {method: 'get'})
+  .then(res => res.json())
+  .then(data => {
     let object = data;
+    cardsCreation(object);
     renderQuestion(object);
   });
 });
 
+function cardsCreation(info) {
+  for (let t = info.nTasks; t > 0; t--) {
+    document.body.innerHTML += `<div class="container">
+      <div class="row">
+        <div class="col s12 m5 l12">
+          <div class="card-panel blue z-depth-3">
+            <span class="prompting white-text center-align flow-text">
+            </span>
+            <a class="btn waves-effect blue waves-light btn-large z-depth-3"></a>
+            <a class="btn waves-effect blue waves-light btn-large z-depth-3"></a>
+            <a class="btn waves-effect blue waves-light btn-large z-depth-3"></a>
+            <a class="btn waves-effect blue waves-light btn-large z-depth-3"></a>
+          </div>
+        </div>
+      </div>
+    </div>`
+  };
+};
+
 function renderQuestion(info) {
-  const prompt = document.getElementById("prompting");
+  const prompts = document.getElementsByClassName("prompting");
   const options = document.getElementsByClassName('btn');
-  prompt.innerHTML = info.question.join('');
-  console.log(options.length);
+  for (let j = 0; j < prompts.length; j++) {
+    prompts[j].innerHTML = info.question[j];
+  };
   for(let i = 0; i < options.length; i++) {
     options[i].innerHTML = info.fOptions[i];
   };
--- quizzzler.js
+++ quizzzler.js
@@ -3,12 +3,13 @@ const fs = require('file-system');
 function renderQuiz() {
   let textTorender = fs.readFileSync('./uploads/quiz.txt', 'utf8');
   if (textTorender != null || undefined) {
-    const qPattern = /(\d. \w+.+)/g;
+    const qPattern = /(\d+(?=.). \w+.+)/g;
     const  falseOptionPattern = /[a-d]. \w+.+/g;
     let question = textTorender.match(qPattern);
     let options = textTorender.match(falseOptionPattern);
+    let nTasks = question.length;
     let questionJSONed = {question: question,
-    fOptions: options};
+    fOptions: options, nTasks: nTasks};
     return questionJSONed;
   }
 }
