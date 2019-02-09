3062aaf7e5fc4c70ff9a67646eae1e5938393d05
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-02-09 11:37:08 +0500

    Stable port fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const appRouter = require('./routes/routes.js');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const usrPath = path.join(__dirname, 'public');
 const favicon = require('serve-favicon');
 
