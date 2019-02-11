8df4c33b24da1be4e4ff599604bc7eb758b76d4a
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-02-11 23:19:27 +0500

    New RegExps and output JSON format

--- quizzzler2.js
+++ quizzzler2.js
@@ -5,16 +5,16 @@ function renderQuiz() {
   if (textTorender != null || undefined) {
     const wholeQuestionPattern = /\d+?\. .+(\r\n(\r\n[a-z||A-Z]\. .+)+)/gm;
     const promptPattern = /\d+?\. .+/;
-    const fOptionPattern = /[a-z||A-Z]\. .+/;
-    const cOptionPattern = /[a-z||A-Z]\. .+!/;
+    const fOptionPattern = /[a-z||A-Z]\. [^!\n\r]+/g;
+    const cOptionPattern = /[a-z||A-Z]\. .+!/g;
     let wholeQuestion = textTorender.match(wholeQuestionPattern);
     const questionJSONed = {};
     for (let i = 0; i < wholeQuestion.length; i++) {
       let question = [];
       question[i] = {};
-      question[i]["prompt"] = textTorender.match(promptPattern);
-      question[i]["fOptions"] = textTorender.match(fOptionPattern);
-      question[i]["cOption"] = textTorender.match(cOptionPattern);
+      question[i]["prompt"] = wholeQuestion[i].match(promptPattern);
+      question[i]["fOptions"] = wholeQuestion[i].match(fOptionPattern);
+      question[i]["cOption"] = wholeQuestion[i].match(cOptionPattern);
       questionJSONed[i] = question[i];
     }
     return questionJSONed;
